e00edb699303fef23d6af4b14d706616ceab139c
Patrick Campbell <patrick@paperlesspcs.com>
2014-12-02 00:37:17 -0600

    Customized front page added devise for login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Heroku
+  # Note to set to acutal host
+  config.action_mailer.default_url_options = { :host => 'patricksproject.herokuapp.com' }
 end
